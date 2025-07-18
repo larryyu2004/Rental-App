ca4fd75c9e2ee3b3b4c1c3d4933ebb5547a798c6
larryyu2004 <jiayiyu@myyahoo.com>
2025-07-18 15:41:38 +0930

    feat: implement sidebar for authenticated users with role-based navigation

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -1,23 +1,43 @@
 "use client";
 
-import Navbar from '@/components/Navbar'
-import { NAVBAR_HEIGHT } from '@/lib/constants'
-import { useGetAuthUserQuery } from '@/state/api'
-import React from 'react'
+import Navbar from "@/components/Navbar";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import { useGetAuthUserQuery } from "@/state/api";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const { data: authUser } = useGetAuthUserQuery();
-  console.log("Auth User:", authUser);
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        (userRole === "manager" && pathname.startsWith("/search")) ||
+        (userRole === "manager" && pathname === "/")
+      ) {
+        router.push("/managers/properties", { scroll: false });
+      } else {
+        setIsLoading(false);
+      }
+    }
+  }, [authUser, router, pathname]);
+
+  if (authLoading || isLoading) return <>Loading...</>;
 
   return (
-    <div className='h-full '>
-        <Navbar />
-        <main className={`h-full flex w-full flex-col`}
+    <div className="h-full w-full">
+      <Navbar />
+      <main
+        className={`h-full flex w-full flex-col`}
         style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}>
-            {children}
-        </main>
+        {children}
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
