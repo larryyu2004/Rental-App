e51e6a14df692b8539c0bf799b16d866088c33eb
larryyu2004 <jiayiyu@myyahoo.com>
2025-08-05 22:27:27 +0930

    feat: implement property details page with image previews, overview, location, and contact widget components

--- client/src/app/(nondashboard)/search/FiltersFull.tsx
+++ client/src/app/(nondashboard)/search/FiltersFull.tsx
@@ -64,6 +64,28 @@ const FiltersFull = () => {
     }));
   };
 
+  const handleLocationSearch = async () => {
+    try {
+      const response = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+          localFilters.location
+        )}.json?access_token=${
+          process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+        }&fuzzyMatch=true`
+      );
+      const data = await response.json();
+      if (data.features && data.features.length > 0) {
+        const [lng, lat] = data.features[0].center;
+        setLocalFilters((prev) => ({
+          ...prev,
+          coordinates: [lng, lat],
+        }));
+      }
+    } catch (err) {
+      console.error("Error search location:", err);
+    }
+  };
+
   if (!isFiltersFullOpen) return null;
 
   return (
@@ -85,7 +107,7 @@ const FiltersFull = () => {
               className="rounded-l-xl rounded-r-none border-r-0"
             />
             <Button
-              // onClick={handleLocationSearch}
+              onClick={handleLocationSearch}
               className="rounded-r-xl rounded-l-none border-l-none border-black shadow-none border hover:bg-primary-700 hover:text-primary-50">
               <Search className="w-4 h-4" />
             </Button>
@@ -221,8 +243,7 @@ const FiltersFull = () => {
                     ? "border-black"
                     : "border-gray-200"
                 )}
-                onClick={() => handleAmenityChange(amenity as AmenityEnum)}
-              >
+                onClick={() => handleAmenityChange(amenity as AmenityEnum)}>
                 <Icon className="w-5 h-5 hover:cursor-pointer" />
                 <Label className="hover:cursor-pointer">
                   {formatEnumString(amenity)}
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -17,7 +17,7 @@ export const api = createApi({
     },
   }),
   reducerPath: "api",
-  tagTypes: ["Managers", "Tenants", "Properties"],
+  tagTypes: ["Managers", "Tenants", "Properties", "PropertyDetails"],
   endpoints: (build) => ({
     getAuthUser: build.query<User, void>({
       queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
@@ -104,6 +104,11 @@ export const api = createApi({
           : [{ type: "Properties", id: "LIST" }],
     }),
 
+    getProperty: build.query<Property, number>({
+      query: (id) => `properties/${id}`,
+      providesTags: (result, error, id) => [{ type: "PropertyDetails", id }],
+    }),
+
     // Tenant Related Endpoints
     getTenant: build.query<Tenant, string>({
       query: (cognitoId) => `tenants/${cognitoId}`,
@@ -157,6 +162,7 @@ export const {
   useUpdateTenantSettingsMutation,
   useUpdateManagerSettingsMutation,
   useGetPropertiesQuery,
+  useGetPropertyQuery,
   useGetTenantQuery,
   useAddFavoritePropertyMutation,
   useRemoveFavoritePropertyMutation,
